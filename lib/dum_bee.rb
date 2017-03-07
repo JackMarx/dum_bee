87bafaf817eb26c7fbb6403d22bc7e6de9f7bb62
JackMarx <seriousfools@gmail.com>
2017-03-07 14:26:47 -0600

    fix buzz

--- lib/dum_bee.rb
+++ lib/dum_bee.rb
@@ -3,7 +3,7 @@ require "dum_bee/version"
 module Dum
   class Bee
     def buzz
-      `zzzzzzzzzzzzzzzzzzzz uh uh zzzzzzz uh zzzzzzzzzzzzzzzzzzzzzzzz uh zzzzzzzzzzz`
+      say 'zzzzzzzzzzzzzzzzzzzz uh uh zzzzzzz uh zzzzzzzzzzzzzzzzzzzzzzzz uh zzzzzzzzzzz'
     end
   end
 end
